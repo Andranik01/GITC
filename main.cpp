15e5ed915faa5ff365231f6bd0c1a89cded86a9c
Andranik Grigoryan <88931722+Andranik01@users.noreply.github.com>
2021-10-20 21:23:50 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include "iostream"
+#include "string"
+
+int main() {
+
+    std::string x = "Hello test esim test ole";
+
+
+    std::cout <<  x.find_first_not_of("test");
+
+
+
+
+//    x.insert(5, " TEST");
+
+//    x.replace(1, 1, "MIKKE");
+
+//    std::cout << x.substr(2);
+
+//    int i = x.find("test");
+//
+//    if ( std::string::npos != i ) {
+//        std::cout << '+';
+//    } else {
+//        std::cout << '-';
+//    }
+
+    return 0;
+}
