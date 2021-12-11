b3db0c7e505396f33154cdf014f78b3b9018043a
Andranik Grigoryan <88931722+Andranik01@users.noreply.github.com>
2021-12-11 10:30:08 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# users = [
+#     {
+#         "name": "John",
+#         "age": 25
+#     },
+#     {
+#         "name": "Vladimir",
+#         "age": 20
+#     },
+#     {
+#         "name": "Kevin",
+#         "age": 30
+#     }
+# ]
+#
+#
+# for i in users:
+#     print(i.get("name"))
